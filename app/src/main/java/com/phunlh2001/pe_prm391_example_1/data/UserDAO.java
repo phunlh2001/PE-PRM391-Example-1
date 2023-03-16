e8210a820743ebe9b96b18705da4afec2c9c9c20
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-16 21:38:49 +0700

    feat: add methods dao

---
+++ app/src/main/java/com/phunlh2001/pe_prm391_example_1/data/UserDAO.java
@@ -0,0 +1,28 @@
+package com.phunlh2001.pe_prm391_example_1.data;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+
+    @Insert
+    void insert(User... user);
+
+    @Update
+    void update(User... user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("SELECT * FROM users")
+    List<User> getAll();
+
+    @Query("SELECT * FROM users WHERE username=:username")
+    List<User> checkUser(String username);
+}
