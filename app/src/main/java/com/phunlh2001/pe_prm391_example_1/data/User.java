0f510f642f428cc90e533a27145e800c58b39f98
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-16 21:37:10 +0700

    feat: add model user

---
+++ app/src/main/java/com/phunlh2001/pe_prm391_example_1/data/User.java
@@ -0,0 +1,43 @@
+package com.phunlh2001.pe_prm391_example_1.data;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "users")
+public class User implements Serializable {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String username;
+    private String address;
+
+    public User(String username, String address) {
+        this.username = username;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
